9152637689d3a01b9f626c48a9b72a8220b7645d
lamya <lamyaaraed24@gmail.com>
2019-06-19 14:43:45 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+int main()
+{
+    Stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    cout << endl << "SIZE = " << s.size() << endl << endl;
+    //while(!s.empty())
+    //{
+    cout << s.top() << endl;
+
+    //}
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+    cout << s.top() << endl;
+    s.pop();
+
+    cout << endl << "SIZE = " << s.size() << endl << endl;
+/*
+    s.push(4);
+    s.push(5);
+    s.push(6);
+
+    cout << s.top() << endl;
+*/
+    return 0;
+}
